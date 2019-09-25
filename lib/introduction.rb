a5f7713ec13390e3c3f22a8103a32176f23b7729
helpful-animation-4719 <um_2003_8@hotmail.com>
2019-09-25 19:31:19 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -17,7 +17,7 @@
 # introduction_with_language("Uma" , "Ruby")
 
 def introduction_with_language_optional(name,language)
-  puts "Hi my name is #{name} and I am learning to program in #{language = "Ruby"}."
+  puts "Hi my name is #{name} and I am learning to program in #{language = "Python"}."
 end
 
 introduction_with_language_optional("Edwin", "Ruby")
